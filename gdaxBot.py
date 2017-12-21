6105841f0e5e3d3f2666632db635ec56ec1eba59
Panayiotis Petousis <pp89@ucla.edu>
2017-12-21 12:49:00 +0200

    Added trading rule

--- gdaxBot.py
+++ gdaxBot.py
@@ -21,6 +21,7 @@ class bot():
         self.buying_status = False
         self.selling_status = False
         self.currency = 'LTC-USD'
+        self.size = 0.01
         self.current_price = None
         self.buying_price = None
         self.selling_price = None
@@ -28,6 +29,9 @@ class bot():
         self.highest_price = None
         self.lowest_price = None
         self.profit = None
+        
+        
+        self.army = None
 
         
     def __call__(self, authenticate=self.authenticate):
@@ -38,26 +42,63 @@ class bot():
         if authenticate:
             army = GdaxArmy().authenticate(api_key = self.api_key, secret_key = self.secret_key,
                                            passphrase = self.passphrase, is_sandbox_url = self.is_sandbox_url)
+            print("Army created, Ready to deploy bots!")
         else:
             army = GdaxArmy()
+            print("Army created, No authentication, cannot deploy bots!")
+            
+        self.army = army
+                
+           
+    def deployBot(self, num_transaction_pairs, percentage_price):
         
-        self.current_price = army.get_currency_price(currency=self.currency)
-        
-        return army
-    
-    def deployBot(self, num_transaction_pairs, percentage_price, army):
         self.percentage_price = percentage_price
         
         print('Bot deployed ...')
         print('Buying @ X price')
+        
         ############################
         #buy and sell rules will go here
         # calculate profit and add
+        
+        self.profit = trade()
         
         ############################
         
         print('Finished transaction pari with X profit')
         
+    def trade():
+        self.current_price = self.army.get_currency_price(currency=self.currency)
         
+        ## buy
+        self.army.buy(price=self.current_price, size=self.size, product_id=self.currency, post_only=True)
+        self.buying_status = True
+        self.buying_price = self.current_price
+        
+        #initialize lowest and highest prices
+        self.lowest_price = self.current_price
+        self.highest_price = self.current_price
+        
+        ############################
+        while !self.selling_status:
+            if self.current_price <= self.lowest_price:
+                self.lowest_price = self.current_price
+                
+            if self.current_price >= self.highest_price:
+                self.highest_price = self.current_price
+                
+            self.current_price = self.army.get_currency_price(currency=self.currency)
+            
+            if self.current_price <= self.highest_price*(1-self.percentage_price) and self.selling_status==True:
+                self.army.buy(price=self.current_price, size=0.01, product_id='LTC-USD', post_only=True)
+                self.buying_status = True
+            elif self.current_price >= self.lowest_price*(1+self.percentage_price):
+                self.army.sell(price=self.current_price, size=0.01, product_id='LTC-USD', post_only=True)
+                self.selling_price = self.current_price
+                self.selling_status = True
+        
+        profit = self.selling_price - self.buying_price
+        
+        return profit
     
     
