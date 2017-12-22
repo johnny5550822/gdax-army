3b842cd70742c5bd2ee8df14e2407e225e113275
Panayiotis Petousis <pp89@ucla.edu>
2017-12-22 22:42:35 +0200

    updated gdaxBot lib

--- gdaxBot.py
+++ gdaxBot.py
@@ -2,21 +2,20 @@
 Bot that performs a one to one transactions.
 """
 
-import utils, time
+from utils import *
 
 class bot():
     """
     An object that uses the util API to perform simple transactions on the gdax platfform.
     """
     
-    def _init_(self, api_base, api_key, secret_key, passphrase, is_sandbox_url, authenticate):
+    def __init__(self, api_key, secret_key, passphrase, is_sandbox_url):
         
-        self.api_base = api_base
         self.api_key = api_key
         self.secret_key = secret_key
         self.passphrase = passphrase
         self.is_sandbox_url = is_sandbox_url
-        self.authenticate = authenticate
+        self.authenticate = False
         
         self.buying_status = False
         self.selling_status = False
@@ -30,18 +29,18 @@ class bot():
         self.lowest_price = None
         self.profit = None
         
-        
         self.army = None
-
         
-    def __call__(self, authenticate=self.authenticate):
+    def __call__(self,authenticate):
         """
         Create a Gdax army object.
         """        
+        self.authenticate = authenticate
         
         if authenticate:
-            army = GdaxArmy().authenticate(api_key = self.api_key, secret_key = self.secret_key,
-                                           passphrase = self.passphrase, is_sandbox_url = self.is_sandbox_url)
+            army = GdaxArmy()
+            army.authenticate(api_key = self.api_key, secret_key = self.secret_key,
+                              passphrase = self.passphrase, is_sandbox_url = self.is_sandbox_url)
             print("Army created, Ready to deploy bots!")
         else:
             army = GdaxArmy()
@@ -51,54 +50,117 @@ class bot():
                 
            
     def deployBot(self, num_transaction_pairs, percentage_price):
+        """
+        Demploying a bot. Rules or models of trading must be defined here.
+        """
+
         
         self.percentage_price = percentage_price
         
         print('Bot deployed ...')
-        print('Buying @ X price')
         
         ############################
         #buy and sell rules will go here
         # calculate profit and add
         
-        self.profit = trade()
+        profit = self.trade()
         
         ############################
         
-        print('Finished transaction pari with X profit')
+        print('Finished transaction pair with ', profit,'profit')
         
-    def trade():
+    def simple_trading_rule(self):
+        self.current_price = self.army.get_currency_price(currency=self.currency)
+        print('Current price',self.current_price)
+
+        ## buy
+        self.buying_trade()
+
+        ## sell
+        self.selling_trade()
+
+        #### reporting profit
+        self.profit = self.profit_report()
+
+        return self.profit    
+        
+    def trade(self):
         self.current_price = self.army.get_currency_price(currency=self.currency)
         
         ## buy
-        self.army.buy(price=self.current_price, size=self.size, product_id=self.currency, post_only=True)
-        self.buying_status = True
-        self.buying_price = self.current_price
+        self.buying_trade()
         
         #initialize lowest and highest prices
         self.lowest_price = self.current_price
         self.highest_price = self.current_price
         
+        #### percentage price
+        self.percentage_price = self.percentage_price/self.current_price
+        
+        profit = 0
+
         ############################
-        while !self.selling_status:
-            if self.current_price <= self.lowest_price:
-                self.lowest_price = self.current_price
+        for i in range(50):
+            
+            self.update_lowest_price()
                 
-            if self.current_price >= self.highest_price:
-                self.highest_price = self.current_price
+            self.update_highest_price()
                 
             self.current_price = self.army.get_currency_price(currency=self.currency)
             
-            if self.current_price <= self.highest_price*(1-self.percentage_price) and self.selling_status==True:
-                self.army.buy(price=self.current_price, size=0.01, product_id='LTC-USD', post_only=True)
-                self.buying_status = True
-            elif self.current_price >= self.lowest_price*(1+self.percentage_price):
-                self.army.sell(price=self.current_price, size=0.01, product_id='LTC-USD', post_only=True)
-                self.selling_price = self.current_price
-                self.selling_status = True
-        
-        profit = self.selling_price - self.buying_price
+            if self.current_price <= self.highest_price*(1+self.percentage_price) and self.selling_status==True:
+                ## buy
+                self.buying_trade()
+                self.selling_status=False
+            elif self.current_price >= self.lowest_price*(1-self.percentage_price) and self.buying_status==True:
+                ## sell
+                self.selling_trade()
+                self.buying_status=False
+                
+                #### reporting profit
+                profit += self.profit_report()
+                
+            print(self.percentage_price)
+            
         
         return profit
     
+    def buying_trade(self):
+        """
+            A Buying transaction.
+        """
+        ## buy
+        self.army.buy(price=self.current_price, size=self.size, product_id=self.currency, post_only=True)
+        self.buying_price = self.current_price
+        self.buying_status = True
+        print('Buying price',self.current_price)
+        
+    def selling_trade(self):
+        """
+            A Buying transaction.
+        """
+        ## sell
+        self.army.sell(price=self.current_price, size=0.01, product_id='LTC-USD', post_only=True)
+        self.selling_price = self.current_price
+        self.selling_status = True
+        print('Selling price',self.current_price)
+        
+    def profit_report(self):
+        """
+            Profits from a pair of buying and selling transaction.
+        """
+        return self.selling_price - self.buying_price
     
+    def update_lowest_price(self):
+        """
+            Updating the lowest price over time.
+        """
+        if self.current_price <= self.lowest_price:
+            self.lowest_price = self.current_price
+            
+    def update_highest_price(self):
+        """
+            Updating the highest price over time.
+        """
+        if self.current_price >= self.highest_price:
+            self.highest_price = self.current_price
