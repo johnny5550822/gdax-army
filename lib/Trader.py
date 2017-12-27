a888d6c5255f7233a645e60f064fa85c9432e449
Johnny Ho <johnny5550822@gmail.com>
2017-12-26 17:44:59 -0800

    upgrade logger in the Trader

--- lib/Trader.py
+++ lib/Trader.py
@@ -3,16 +3,6 @@ from lib import GdaxArmy, BuyStrategier, SellStrategier
 import time
 import logging
 
-# timestamp
-time_str = str(time.asctime(time.localtime(time.time()))).replace(' ', '_')
-
-# general logger
-logger = setup_logger(__name__, 'logs/%s_log.log' % time_str)
-
-# deal logger that only only log the successful trade
-trade_logger = setup_logger(__name__ + '_trade',
-                            'logs/trade.log')
-
 
 class Trader():
     """
@@ -27,11 +17,18 @@ class Trader():
                  macd_short_n=12, macd_long_n=26,
                  trade_option=1,
                  ):
+        # setup logger
+        time_str = str(time.asctime(time.localtime(time.time()))
+                       ).replace(' ', '_')
+        self.logger = setup_logger(__name__, 'logs/%s_log.log' % time_str)
+        self.trade_logger = setup_logger(__name__ + '_trade',
+                                         'logs/%s_trade.log' % time_str)
+
         # logger
-        logger.info('')
-        logger.info('---------------New trade---------------')
-        trade_logger.info('')
-        trade_logger.info('---------------New trade---------------')
+        self.logger.info('')
+        self.logger.info('---------------New trade---------------')
+        self.trade_logger.info('')
+        self.trade_logger.info('---------------New trade---------------')
 
         # api key
         self.api_key = api_key
@@ -65,15 +62,15 @@ class Trader():
                                            )
         self.sellStrategier = SellStrategier(self.army, currency, granularity,
                                              num_buckets, term_n,
-                                             macd_short_n, macd_long_n, 
+                                             macd_short_n, macd_long_n,
                                              time_str)
 
         # trade options
         self.trade_option = trade_option
-        logger.info('Trade Option:%s' % self.trade_option)
+        self.logger.info('Trade Option:%s' % self.trade_option)
 
         # log the configuration
-        logger.info("Initial Account Summary:%s" % self.acct_summary)
+        self.logger.info("Initial Account Summary:%s" % self.acct_summary)
 
     def trade(self):
         """
@@ -84,18 +81,18 @@ class Trader():
         Note: for now, the algorithm will first&end CANCELL all the existing 
         orders, which may cause 'leftover' in digital coin (e.g., LTC).
         """
-        logger.info('########## Trade ##########')
+        self.logger.info('########## Trade ##########')
 
         # wait until the pattern favor not to buy (so that we can catch the
         # moment when it is good to buy in the loop).
         while self.buyStrategier.should_buy(option=self.trade_option):
-            logger.info('Waiting price<ema to start trading cycle.')
+            self.logger.info('Waiting price<ema to start trading cycle.')
             time.sleep(2)  # not overwhelming the api
             break
 
         # loop
         while True:
-            logger.info('Start trading loop......')
+            self.logger.info('Start trading loop......')
 
             # security check to make sure it is safe to start investment
             try:
@@ -115,8 +112,8 @@ class Trader():
                             trade_option=self.trade_option)
                         if not is_bought:
                             self._clean_an_order(buy_order)
-                        logger.info('Bought?:%s' % is_bought)
-                    logger.info('Bought order:%s' % buy_order)
+                        self.logger.info('Bought?:%s' % is_bought)
+                    self.logger.info('Bought order:%s' % buy_order)
                     self._log_trade(buy_order)
 
                     # excute sell stragegy
@@ -128,12 +125,12 @@ class Trader():
                             trade_option=self.trade_option)
                         if not is_sold:
                             self._clean_an_order(sell_order)
-                        logger.info('Sold?:%s' % is_sold)
-                    logger.info('Sold order:%s' % sell_order)
+                        self.logger.info('Sold?:%s' % is_sold)
+                    self.logger.info('Sold order:%s' % sell_order)
                     self._log_trade(sell_order)
 
             except Exception, e:
-                logger.info("Exception:%s" % e)
+                self.logger.info("Exception:%s" % e)
 
     def _clean_all_orders(self):
         """
@@ -142,7 +139,7 @@ class Trader():
         Note: if the trading pricinple is a-buy-a-sell, there should be only 
         <=1 existing order.
         """
-        logger.info('Cleanning all orders......')
+        self.logger.info('Cleanning all orders......')
 
         orders = self.army.get_orders()
         orders = orders[0]
@@ -156,7 +153,7 @@ class Trader():
         Clean a placed order.
         """
         if order is not None and order['id'] is not none:
-            logger.info('Cancel a placed order...')
+            self.logger.info('Cancel a placed order...')
             self.army.cancel_order(order['id'])
 
     def _determine_order_price(self, pos=3, order_type='bids'):
@@ -179,7 +176,7 @@ class Trader():
         :params: time_limit: how much time we wait for the excution
         :params: trade strategy option
         """
-        logger.info('Executing buy order ...... ')
+        self.logger.info('Executing buy order ...... ')
 
         # check if should sell.
         if self.buyStrategier.should_buy(option=trade_option):
@@ -189,7 +186,7 @@ class Trader():
                                   product_id=self.currency)
 
             if self._is_order_placed(order):
-                logger.info('Placed a buy order.')
+                self.logger.info('Placed a buy order.')
                 is_bought = self._wait_order_complete(order['id'],
                                                       pause_time, time_limit)
                 return is_bought, order
@@ -208,7 +205,7 @@ class Trader():
         :params: time_limit: how much time we wait for the excution
         :params: trade strategy option
         """
-        logger.info('Executing sell order ...... ')
+        self.logger.info('Executing sell order ...... ')
 
         # check if should sell
         if self.sellStrategier.should_sell(buy_order=order,
@@ -219,21 +216,21 @@ class Trader():
                                         product_id=self.currency)
 
             if self._is_order_placed(order):
-                logger.info('Placed a sell order.')
+                self.logger.info('Placed a sell order.')
                 is_sold = self._wait_order_complete(sell_order['id'],
                                                     pause_time, time_limit)
                 if not is_sold:
-                    logger.info('Not sell sell order. Consider resell.')
+                    self.logger.info('Not sell sell order. Consider resell.')
                     self.army.cancel_order(sell_order['id'])
                     if self.sellStrategier.should_resell(order):
-                        logger.info('Resell.')
+                        self.logger.info('Resell.')
                         new_order = self.army.sell(product_id=self.currency,
                                                    price=price,
                                                    size=self.size,
                                                    type='market',
                                                    post_only=False)
                         new_order['price'] = price  # they missed this info
-                        logger.info("Sell at market price, I am a taker!")
+                        self.logger.info("Sell at market price, I am a taker!")
                         return True, new_order
                 return is_sold, order
         return False, None
@@ -287,18 +284,18 @@ class Trader():
 
         :params order: the order
         """
-        trade_logger.info('trade_option%s\t%s\t%s\t%s\t%s'
-                          % (self.trade_option,
-                             order['product_id'],
-                             order['side'],
-                             order['price'],
-                             order['size']))
+        self.trade_logger.info('trade_option%s\t%s\t%s\t%s\t%s'
+                               % (self.trade_option,
+                                  order['product_id'],
+                                  order['side'],
+                                  order['price'],
+                                  order['size']))
 
     def _pass_initial_check(self):
         """
         excute pre-trade check to make sure we are safe to trade
         """
-        logger.info('Doing initial check......')
+        self.logger.info('Doing initial check......')
 
         # manual defined threshold
         value_limit = self.value_limit
@@ -312,8 +309,8 @@ class Trader():
         # value.
         initial_value = self._sum_coin_summary(self.acct_summary)
         current_value = self._sum_coin_summary(acct_summary)
-        logger.info('Current Acct info: %s' % acct_summary)
-        logger.info('Current total value: %s' % current_value)
+        self.logger.info('Current Acct info: %s' % acct_summary)
+        self.logger.info('Current total value: %s' % current_value)
         if (initial_value * percent_remain_limit > current_value):
             return False
         if current_value < value_limit:
@@ -346,12 +343,13 @@ class Trader():
         """
         while not self._is_order_filled(id=id):
             if (time_limit % 20 == 0):
-                logger.info('Order complete remaining time:%s' % time_limit)
+                self.logger.info(
+                    'Order complete remaining time:%s' % time_limit)
 
             time.sleep(pause_time)  # pause for x second
             time_limit -= pause_time
             if time_limit <= 0:
                 return False
 
-        logger.info("Completed order at:%ss" % time_limit)
+        self.logger.info("Completed order at:%ss" % time_limit)
         return True
